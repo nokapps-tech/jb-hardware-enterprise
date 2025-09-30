d9497a097cb0ee37e1044af4f486b2f735e8076d
Paul Bryan <paulbryanco.nokapps@gmail.com>
2025-10-01 00:09:42 +0800

    feature: improved show in suppliers

--- app/Livewire/Suppliers/Show.php
+++ app/Livewire/Suppliers/Show.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Suppliers;
 
 use App\Livewire\Forms\SupplierForm;
 use App\Models\Supplier;
+use App\Models\Storage1Transaction;
+use App\Models\Storage2Transaction;
 use Livewire\Component;
 
 class Show extends Component
@@ -17,6 +19,15 @@ class Show extends Component
 
     public function render()
     {
-        return view('livewire.supplier.show', ['supplier' => $this->form->supplierModel]);
+        $supplier = $this->form->supplierModel;
+
+        $storage1Transactions = Storage1Transaction::where('supplier_id', $supplier->id)->get();
+        $storage2Transactions = Storage2Transaction::where('supplier_id', $supplier->id)->get();
+
+        return view('livewire.supplier.show', [
+            'supplier' => $supplier,
+            'storage1Transactions' => $storage1Transactions,
+            'storage2Transactions' => $storage2Transactions,
+        ]);
     }
 }
