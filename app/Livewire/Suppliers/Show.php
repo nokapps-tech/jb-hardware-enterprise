299353a9d339f920c74434e526a3484b1e6c65f0
Paul Bryan <paulbryanco.nokapps@gmail.com>
2025-11-03 11:28:09 +0800

    hotfix: removed old transaction module

--- app/Livewire/Suppliers/Show.php
+++ app/Livewire/Suppliers/Show.php
@@ -4,8 +4,6 @@ namespace App\Livewire\Suppliers;
 
 use App\Livewire\Forms\SupplierForm;
 use App\Models\Supplier;
-use App\Models\Storage1Transaction;
-use App\Models\Storage2Transaction;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -28,16 +26,12 @@ class Show extends Component
             ? null
             : $user->branches->pluck('id');
 
-        $storage1Transactions = Storage1Transaction::where('supplier_id', $supplier->id)->get();
-        $storage2Transactions = Storage2Transaction::where('supplier_id', $supplier->id)->get();
         $transactions = Transaction::where('supplier_id', $supplier->id)
             ->when($branchIds, fn($q) => $q->whereIn('branch_id', $branchIds))
             ->get();
 
         return view('livewire.supplier.show', [
             'supplier' => $supplier,
-            'storage1Transactions' => $storage1Transactions,
-            'storage2Transactions' => $storage2Transactions,
             'transactions' => $transactions,
         ]);
     }
